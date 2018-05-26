6b7ad6e8aaa889a5ba6df1c6bf0504b222de7772
Jebisan Hari Nadarajah <jenad14@student.sdu.dk>
2018-05-26 20:12:31 +0200

    Small changes and adjustments etc.

--- UmbracoHomework/Controllers/HomeController.cs
+++ UmbracoHomework/Controllers/HomeController.cs
@@ -18,18 +18,6 @@ namespace UmbracoHomework.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+       
     }
 }
--- UmbracoHomework/Controllers/SubmissionsController.cs
+++ UmbracoHomework/Controllers/SubmissionsController.cs
@@ -53,22 +53,22 @@ namespace UmbracoHomework.Controllers
         }
 
         // GET: Submissions/Details/5
-        public ActionResult Details(int? id)
-        {
-            using (var db = new AcmeDBEntities())
-            {
-                if (id == null)
-                {
-                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-                }
-                Submission submission = db.SubmissionSet.Find(id);
-                if (submission == null)
-                {
-                    return HttpNotFound();
-                }
-                return View(submission);
-            }
-        }
+        //public ActionResult Details(int? id)
+        //{
+        //    using (var db = new AcmeDBEntities())
+        //    {
+        //        if (id == null)
+        //        {
+        //            return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //        }
+        //        Submission submission = db.SubmissionSet.Find(id);
+        //        if (submission == null)
+        //        {
+        //            return HttpNotFound();
+        //        }
+        //        return View(submission);
+        //    }
+        //}
 
         // GET: Submissions/Create
         public ActionResult Create()
@@ -124,37 +124,37 @@ namespace UmbracoHomework.Controllers
 }
 
 // GET: Submissions/Edit/5
-public ActionResult Edit(int? id)
-{
-    if (id == null)
-    {
-        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-    }
-    Submission submission = db.SubmissionSet.Find(id);
-    if (submission == null)
-    {
-        return HttpNotFound();
-    }
-    ViewBag.Serialnumber = new SelectList(db.Serialnumber, "Serialnumber1", "Serialnumber1", submission.Serialnumber);
-    return View(submission);
-}
+//public ActionResult Edit(int? id)
+//{
+//    if (id == null)
+//    {
+//        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+//    }
+//    Submission submission = db.SubmissionSet.Find(id);
+//    if (submission == null)
+//    {
+//        return HttpNotFound();
+//    }
+//    ViewBag.Serialnumber = new SelectList(db.Serialnumber, "Serialnumber1", "Serialnumber1", submission.Serialnumber);
+//    return View(submission);
+//}
 
 // POST: Submissions/Edit/5
 // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
 // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-[HttpPost]
-[ValidateAntiForgeryToken]
-public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,Age,Serialnumber,DOB")] Submission submission)
-{
-    if (ModelState.IsValid)
-    {
-        db.Entry(submission).State = EntityState.Modified;
-        db.SaveChanges();
-        return RedirectToAction("Index");
-    }
-    ViewBag.Serialnumber = new SelectList(db.Serialnumber, "Serialnumber1", "Serialnumber1", submission.Serialnumber);
-    return View(submission);
-}
+//[HttpPost]
+//[ValidateAntiForgeryToken]
+//public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,Email,Age,Serialnumber,DOB")] Submission submission)
+//{
+//    if (ModelState.IsValid)
+//    {
+//        db.Entry(submission).State = EntityState.Modified;
+//        db.SaveChanges();
+//        return RedirectToAction("Index");
+//    }
+//    ViewBag.Serialnumber = new SelectList(db.Serialnumber, "Serialnumber1", "Serialnumber1", submission.Serialnumber);
+//    return View(submission);
+//}
 
 // GET: Submissions/Delete/5
 public ActionResult Delete(int? id)
